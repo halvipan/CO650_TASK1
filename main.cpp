6222bb1a84800090ee40e48cd6b0038b8122e823
Hal Vipan <hal.vipan@sky.uk>
2024-08-25 19:32:59 +0100

    Complete Task

--- main.cpp
+++ main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include "GameComponent.h"
 #include "DrawableGameComponent.h"
+#include "Game.h"
 
 int GameComponent::instances = 0;
 
+void init() {
+    cout << "Initialising game" << endl;
+}
+
+void term() {
+    cout << "Terminating game" << endl;
+}
+
 int main() {
-    GameComponent* gc1 = new GameComponent();
-    GameComponent* gc2 = new GameComponent();
-    GameComponent* gc3 = new GameComponent();
-
-    time_t t = time(nullptr);
-    tm* time = localtime(&t);
-
-    gc1->Update(time);
-    gc2->Update(time);
-    gc3->Update(time);
-
-    DrawableGameComponent* dgc1 = new DrawableGameComponent(1,1);
-
-    dgc1->Update(time);
-    dgc1->Update(time);
-    dgc1->Update(time);
+    Game* game = new Game(2);
+    game->SetInitialise(init);
+    game->SetTerminate(term);
+    game->Add(new GameComponent());
+    game->Add(new DrawableGameComponent(0, 0));
+    game->Run();
 }
